dca4b2e8838ed745a080cc002fc4f0b6ac48a5f6
Piotr Klimiec <klimiec@gmail.com>
2016-11-15 23:33:25 +0100

    Firefox support for Linux and Windows. Upgrade selenium  version 3.0.1

--- src/main/java/common/browser/FirefoxBrowser.java
+++ src/main/java/common/browser/FirefoxBrowser.java
@@ -9,16 +9,33 @@ import java.io.File;
 
 public class FirefoxBrowser extends BrowserAbstract{
 
-    private static final String FIREFOXDRIVER_PATH = "src/main/resources/drivers/firefox/geckodriver.exe";
+    private static final String CHROME_DRIVER_PATH_FORMAT = "src/main/resources/drivers/firefox/firefoxdriver_%s";
+    private static final String FIREFOXDRIVER_PATH_LINUX = String.format(CHROME_DRIVER_PATH_FORMAT, "linux64/geckodriver");
+    private static final String FIREFOXDRIVER_PATH_WINDOWS = String.format(CHROME_DRIVER_PATH_FORMAT, "win32/geckodriver.exe");
     private static final String FIREFOXDRIVER_PROPERTY = "webdriver.gecko.driver";
 
 
     public WebDriver getInstance() {
 
         if (StringUtils.isEmpty(System.getProperty(FIREFOXDRIVER_PROPERTY))) {
-            System.setProperty(FIREFOXDRIVER_PROPERTY, new File(FIREFOXDRIVER_PATH).getPath());
+            String chromeBinaryPath = chromeDriverPathForOS();
+            System.setProperty(FIREFOXDRIVER_PROPERTY, new File(chromeBinaryPath).getPath());
         }
 
         return new FirefoxDriver();
     }
+
+    private String chromeDriverPathForOS() {
+        String operatingSystem = System.getProperty("os.name").toUpperCase();
+
+        if(operatingSystem.contains("WINDOWS")) {
+            return FIREFOXDRIVER_PATH_WINDOWS;
+        }else if(operatingSystem.contains("LINUX")) {
+            return FIREFOXDRIVER_PATH_LINUX;
+        }
+
+        return "";
+    }
+
+
 }
